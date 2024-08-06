29688f9d849c97d56087a86b8a70f199337973bc
avishay farkash <62483176+avishf7@users.noreply.github.com>
2024-08-06 21:11:07 +0300

    add toggle feature and documetation

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,17 +1,20 @@
 import { useTasksContext } from "../contex/TasksContex";
 import { Container, WhiteDiv } from "../styles/taskListStyles";
+import { TaskData } from "../utils/types";
 import Task from "./Task";
 
 /**
  * TaskList component displays a list of tasks.
+ * It fetches tasks from the tasks context and renders each task using the Task component.
+ * It also shows loading and error messages when applicable.
  */
 const TaskList = () => {
     const { tasks, loading, error } = useTasksContext()
 
     return ( 
         <Container>
-            {tasks && tasks.map((desc, i) => 
-                (<Task key={`${i}${desc}`} index={i} desc={desc}/>))
+            {tasks && tasks.map((task : TaskData, i) => 
+                (<Task key={`${i}${task.desc}`} index={i} task={task}/>))
             }       
             {loading && <WhiteDiv>Loading...</WhiteDiv>}
             {error && <WhiteDiv>{ error }</WhiteDiv>}
